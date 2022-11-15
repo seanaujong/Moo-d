5577dc4463f0f5c733dd0b73970f1c1405218cfb
seanaujong <seanaujong@gmail.com>
2022-11-15 00:25:45 -0600

    hide aggregate button if empty

--- app/src/main/java/com/smhvincent/moo_d/moodlist/MoodListFragment.kt
+++ app/src/main/java/com/smhvincent/moo_d/moodlist/MoodListFragment.kt
@@ -1,7 +1,6 @@
 package com.smhvincent.moo_d.moodlist
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +8,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
-import com.smhvincent.moo_d.R
 import com.smhvincent.moo_d.database.MoodDatabase
 import com.smhvincent.moo_d.databinding.FragmentMoodListBinding
 
@@ -45,10 +43,10 @@ class MoodListFragment : Fragment() {
         })
         binding.moodList.adapter = adapter
 
-        moodListViewModel.moods.observe(viewLifecycleOwner) {
-            it?.let {
-                adapter.submitMoodList(it)
-            }
+        moodListViewModel.moods.observe(viewLifecycleOwner) { list ->
+            adapter.submitMoodList(list)
+            binding.navAggregateButton.visibility =
+                if (list.isEmpty()) View.INVISIBLE else View.VISIBLE
         }
 
         // buttons

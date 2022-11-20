a3549a78f481990d4cd55848621b86f450cf7cfe
seanaujong <seanaujong@gmail.com>
2022-11-20 01:18:54 -0600

    animation in aggregate

--- app/src/main/java/com/smhvincent/moo_d/moodaggregate/MoodAggregateFragment.kt
+++ app/src/main/java/com/smhvincent/moo_d/moodaggregate/MoodAggregateFragment.kt
@@ -1,15 +1,19 @@
 package com.smhvincent.moo_d.moodaggregate
 
+import android.animation.ObjectAnimator
+import android.animation.PropertyValuesHolder
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.lifecycle.ViewModelProvider
 import com.smhvincent.moo_d.R
 import com.smhvincent.moo_d.database.MoodDatabase
 import com.smhvincent.moo_d.databinding.FragmentMoodAggregateBinding
 import kotlin.math.ceil
+
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -35,11 +39,11 @@ class MoodAggregateFragment : Fragment() {
         moodAggregateViewModel.average.observe(viewLifecycleOwner) { average ->
             val roundup = ceil(average).toInt()
             val text = when (roundup) {
-                1 -> getString(R.string.average_1)
-                2 -> getString(R.string.average_2)
-                3 -> getString(R.string.average_3)
-                4 -> getString(R.string.average_4)
-                5 -> getString(R.string.average_5)
+                1 -> getString(com.smhvincent.moo_d.R.string.average_1)
+                2 -> getString(com.smhvincent.moo_d.R.string.average_2)
+                3 -> getString(com.smhvincent.moo_d.R.string.average_3)
+                4 -> getString(com.smhvincent.moo_d.R.string.average_4)
+                5 -> getString(com.smhvincent.moo_d.R.string.average_5)
                 else -> ""
             }
             val image = when (roundup) {
@@ -50,18 +54,33 @@ class MoodAggregateFragment : Fragment() {
                 5 -> R.drawable.cow_5
                 else -> R.drawable.cow_1
             }
-            binding.firstAggregationTv.text = getString(R.string.first_aggregation_message, text)
+            binding.firstAggregationTv.text =
+                getString(com.smhvincent.moo_d.R.string.first_aggregation_message, text)
             binding.secondAggregationIv.setImageResource(image)
         }
 
         moodAggregateViewModel.exercise.observe(viewLifecycleOwner) { exercise ->
             binding.thirdAggregationTv.text =
-                getString(R.string.third_aggregation_message, exercise)
+                getString(com.smhvincent.moo_d.R.string.third_aggregation_message, exercise)
         }
 
         moodAggregateViewModel.func.observe(viewLifecycleOwner) { func ->
-            binding.fourthAggregationTv.text = getString(R.string.fourth_aggregation_message, func)
+            binding.fourthAggregationTv.text =
+                getString(com.smhvincent.moo_d.R.string.fourth_aggregation_message, func)
         }
+
+        val scaleDown: ObjectAnimator = ObjectAnimator.ofPropertyValuesHolder(
+            binding.secondAggregationIv,
+            PropertyValuesHolder.ofFloat("scaleX", 1.2f),
+            PropertyValuesHolder.ofFloat("scaleY", 1.2f)
+        )
+        scaleDown.duration = 310
+
+        scaleDown.repeatCount = ObjectAnimator.INFINITE
+        scaleDown.repeatMode = ObjectAnimator.REVERSE
+        scaleDown.interpolator = FastOutSlowInInterpolator()
+
+        scaleDown.start()
 
         return binding.root
 
